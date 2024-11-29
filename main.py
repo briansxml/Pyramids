121ecd07481e1aca04359f025c195ded0016c7b2
Artem <artemyo17@gmail.com>
2024-11-29 19:29:01 +0300

    Added random colors and UI in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import random
 import sys
 
@@ -5,11 +6,47 @@ from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
+template = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>800</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="pushButton">
+    <property name="geometry">
+     <rect>
+      <x>360</x>
+      <y>530</y>
+      <width>75</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Кружочек</string>
+    </property>
+   </widget>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
         self.setFixedSize(800, 600)
@@ -30,17 +67,17 @@ class Example(QMainWindow):
         size1 = random.randint(20, 100)
         size2 = random.randint(20, 100)
         size3 = random.randint(20, 100)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(random.randint(0, self.width()),
-                    random.randint(0, self.height()), size1,
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        qp.drawEllipse(random.randint(50, self.width() - 50),
+                    random.randint(50, self.height() - 50), size1,
                     size1)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(random.randint(0, self.width()),
-                    random.randint(0, self.height()), size2,
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        qp.drawEllipse(random.randint(50, self.width() - 50),
+                    random.randint(50, self.height() - 50), size2,
                     size2)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(random.randint(0, self.width()),
-                    random.randint(0, self.height()), size3,
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        qp.drawEllipse(random.randint(50, self.width() - 50),
+                    random.randint(50, self.height() - 50), size3,
                     size3)
 
 
